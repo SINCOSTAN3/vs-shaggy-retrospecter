5a9c1428cd50b39d805886fe1327cffd433c6b15
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-03-14 13:09:05 +0200

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -137,6 +137,10 @@ class MainMenuState extends MusicBeatState
 
 		changeItem();
 
+                #if android
+	        addVirtualPad(UP_DOWN, A_B);
+                #end
+
 		super.create();
 	}
 
